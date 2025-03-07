567c79b3b75eec3a987d8484a28372018962d700
Abdul Sattar <absattar2053@gmail.com>
2025-03-08 00:56:59 +0500

    add project images and hero Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
+import Hero from "./components/Hero/Index"
 import NavBar from "./components/NavBar/Index"
 
 const App = () => {
   return (
     <>
     <NavBar/>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+        <Hero/>
     </>
   )
 }
